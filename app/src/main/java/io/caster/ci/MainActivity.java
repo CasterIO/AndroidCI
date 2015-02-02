ed3e6ab87382adc570b0ebad1fd443ba872f9242
Donn Felker <donn@donnfelker.com>
2015-02-02 15:19:58 -0700

    test

--- app/src/main/java/io/caster/ci/MainActivity.java
+++ app/src/main/java/io/caster/ci/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // This is a test
     }
 
 
